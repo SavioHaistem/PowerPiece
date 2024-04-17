c10b50487bcfee8deac27510dc9ad2bb1d1150b7
SavioHaistem <savio.dantas.5858@gmail.com>
2024-04-17 09:46:00 -0300

    feat: sqlException

---
+++ src/db/DB.java
@@ -0,0 +1,15 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DB {
+    Connection connection = null;
+    public void connection() {
+        try {
+
+        } catch (SQLException e) {
+            t
+        }
+    }
+}
