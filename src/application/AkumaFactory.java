a6b796c7cab6bf86f70fce83696dc7bc26bc1b16
SavioHaistem <savio.dantas.5858@gmail.com>
2024-04-17 21:09:41 -0300

    Connection test finalized

--- src/application/AkumaFactory.java
+++ src/application/AkumaFactory.java
@@ -1,4 +1,7 @@
 package application;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class AkumaFactory {
     public static void main(String[] args) {
--- src/db/DB.java
+++ src/db/DB.java
@@ -1,15 +1,25 @@
 package db;
-
+import exceptions.DbException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DB {
     Connection connection = null;
-    public void connection() {
+    public void connection() throws DbException {
         try {
-
+            Class.forName("org.sqlite.JDBC");
+            if (connection == null) {
+                String url = "jdbc:sqlite:/home/savio/akumas.db";
+                connection = DriverManager.getConnection(url);
+                System.out.println(connection);
+            }
         } catch (SQLException e) {
-            t
+            throw new DbException(e.getMessage());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("classForNameError");
         }
     }
 }
