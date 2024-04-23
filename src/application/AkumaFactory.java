1a1d05b443734b19f86ec67066873a6069f471b8
SavioHaistem <savio.dantas.5858@gmail.com>
2024-04-23 10:57:12 -0300

    test(blackbeard)

--- src/application/AkumaFactory.java
+++ src/application/AkumaFactory.java
@@ -1,5 +1,6 @@
 package application;
 import db.DB;
+import entities.enemies.BlackBeard;
 import exceptions.DbException;
 
 import java.sql.Connection;
@@ -8,8 +9,7 @@ import java.sql.SQLException;
 
 public class AkumaFactory {
     public static void main(String[] args) {
-        Connection connection = DB.getConnection();
-        System.out.println(DB.getAkumas());
-        DB.closeConnection(connection);
+        BlackBeard blackBeard = new BlackBeard();
+        System.out.println(blackBeard);
     }
 }
