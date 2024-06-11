0b7aa10155e83f7465824220c7e03a3c16758e82
SavioHaistem <savio.dantas.5858@gmail.com>
2024-06-11 16:48:17 -0300

    feat: instantiate power from stringID

---
+++ src/services/InstantiateFromString.java
@@ -0,0 +1,26 @@
+package services;
+
+import db.DB;
+import db.dao.DaoFactory;
+import db.dao.PowerDao;
+import db.dao.impl.PowerDaoJdbc;
+import entities.models.Power;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InstantiateFromString {
+    public static Map<Integer, Power> powerMap(String stringIDs) {
+        PowerDaoJdbc powerDao = DaoFactory.createPowerDao();
+        Map<Integer,Power> powerMap = new HashMap<>();
+        String[] powersIds = stringIDs.split(",");
+
+        for (String id : powersIds) {
+            int intID = Integer.parseInt(id);
+            powerMap.put(intID,powerDao.findById(intID));
+        }
+
+        powerDao.closeConnection();
+        return powerMap;
+    }
+}
