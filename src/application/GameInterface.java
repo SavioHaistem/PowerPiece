2ea5e08e04d01d7bc97936fcc14c00421e340fcb
SavioHaistem <savio.dantas.5858@gmail.com>
2024-06-18 20:17:53 -0300

    refactor: migrating project to maven structure

--- src/application/GameInterface.java
+++
@@ -1,9 +0,0 @@
-package application;
-import db.dao.DaoFactory;
-import db.dao.EnemyDao;
-
-public class GameInterface {
-    public static void main(String[] args) {
-        EnemyDao enemyDao = DaoFactory.createEnemyDao();
-    }
-}
--- src/interfaces/Fruit.java
+++
@@ -1,5 +0,0 @@
-package interfaces;
-
-public interface Fruit {
-    void Eat();
-}
