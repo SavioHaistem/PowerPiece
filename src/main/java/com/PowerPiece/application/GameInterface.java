fa0c91373ddf8cd84f57c4d801ceffea0b787f49
SavioHaistem <savio.dantas.5858@gmail.com>
2024-06-20 10:47:29 -0300

    removed hibernate

--- src/main/java/com/PowerPiece/application/GameInterface.java
+++ src/main/java/com/PowerPiece/application/GameInterface.java
@@ -1,8 +1,6 @@
 package com.PowerPiece.application;
-
 import com.PowerPiece.db.dao.DaoFactory;
 import com.PowerPiece.db.dao.PowerDao;
-import com.PowerPiece.db.dao.impl.PowerDaoJdbc;
 
 public class GameInterface {
     public static void main(String[] args) {
