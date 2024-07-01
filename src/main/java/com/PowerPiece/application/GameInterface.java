59e80b270fbb4f7bdebc2905631422ef79c0c698
SavioHaistem <savio.dantas.5858@gmail.com>
2024-07-01 14:03:33 -0300

    feat(interfaceService): Question method and title method

--- src/main/java/com/PowerPiece/application/GameInterface.java
+++ src/main/java/com/PowerPiece/application/GameInterface.java
@@ -2,6 +2,7 @@ package com.PowerPiece.application;
 import com.PowerPiece.entities.Player;
 import com.PowerPiece.entities.TextDecorations;
 import com.PowerPiece.services.CacheService;
+import com.PowerPiece.services.InterfaceService;
 
 import java.util.Scanner;
 
@@ -9,13 +10,10 @@ public class GameInterface {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Player player = new Player();
-        System.out.println(TextDecorations.BG_PURPLE + "|| POWER PIECE 1.0 ||" + TextDecorations.RESET);
-        System.out.println(TextDecorations.RESET);
-        System.out.print(TextDecorations.PURPLE + "Choose your player name: ");
-        player.setName(scan.nextLine());
-        System.out.println(TextDecorations.RESET);
 
-        System.out.println(TextDecorations.GREEN + "Hi " + player.getName() + TextDecorations.RESET);
-        System.out.println(TextDecorations.RESET);
+        InterfaceService.title("Power Piece 1.0");
+        InterfaceService.anyQuestion("Choose your nick name: ");
+        player.setName(scan.nextLine());
+        InterfaceService.anyQuestion("Choose one AkumaNoMI: ");
     }
 }
--- src/main/java/com/PowerPiece/services/InterfaceService.java
+++ src/main/java/com/PowerPiece/services/InterfaceService.java
@@ -1,7 +1,17 @@
 package com.PowerPiece.services;
 
+import com.PowerPiece.entities.TextDecorations;
+
 public final class InterfaceService {
     public static void timer(Integer seconds) {
 
+    }
+    public static void title(String title) {
+        System.out.println(TextDecorations.BG_PURPLE + title);
+        System.out.println(TextDecorations.RESET);
+    }
+    public static void anyQuestion(String question) {
+        System.out.println(TextDecorations.PURPLE + "| " + question);
+        System.out.print("> " + TextDecorations.RESET);
     }
 }
